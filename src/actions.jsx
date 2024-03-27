46e97d307944068ec80f0122aaaad31e385aef8f
Daniel Ji <daniel.ji0923@gmail.com>
2024-03-26 19:03:50 -0700

    update and delete

--- src/actions.jsx
+++ src/actions.jsx
@@ -29,5 +29,42 @@ export const createAction = async ({request}) => {
 
 
 // Update Action
+export const updateAction = async ({request, params}) => {
+    // get the form data
+    const formData = await request.formData()
+    
+    // get the pet id
+    const id = params.id
+    
+    // construct the request body
+    const updatedPet = {
+        name: formData.get('name'),
+        age: formData.get('age'),
+    }
 
-// Delete Action
+    // send the request body to the backend
+    await fetch(`${URL}${id}/`, {
+        method: 'PUT',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(updatedPet)
+    })
+
+    // redirect to the show page
+    return redirect(`/pets/${id}`)
+}
+
+// Delete Action -- Rehome a pet
+export const deleteAction = async ({params}) => {
+    // get the pet id
+    const id = params.id
+    
+    // send the request to the backend
+    await fetch(`${URL}${id}/`,{
+        method: 'DELETE'
+    })
+    
+    // redirect to the index page
+    return redirect('/')
+}
--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -1,12 +1,41 @@
-import { Link, useLoaderData } from "react-router-dom"
+import { Link, useLoaderData, Form } from "react-router-dom"
 
 function Show(props) {
     
     const aPet = useLoaderData()
+    const id = aPet?.url.split('/')[4]
+    // ?. optional chaining
     return (
         <div>
             <h1>{aPet.name}</h1>
             <h2>{aPet.age}</h2>
+            
+            <div>
+                {/* Update Form */}
+                <Form action={`/update/${id}`} method='POST'>
+                    <label htmlFor='name'>
+                        Rename your Turtle
+                        <input type='text' id='name' name='name' defaultValue={aPet.name}/>
+                    </label>
+                    
+                    <label htmlFor='age'>
+                        Reage your Turtle
+                        <input type='number' id='age' name='age' defaultValue={aPet.age}/>
+                    </label>
+                    <button style={{"backgroundColor": "blue"}}>
+                        Update Our Turtle
+                    </button>
+                </Form>
+                
+                {/* Delete Form */}
+                <Form action={`/delete/${id}`} method='post'>
+                    <button style={{"backgroundColor": "red"}}>
+                        Release Turtle
+                    </button>
+                </Form>
+            </div>
+            
+            
             <Link to='/'>
                 <button>
                     Go Back
